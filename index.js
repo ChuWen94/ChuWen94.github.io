fd503c4e67de28ecf830514c4f647a8aa2cd6e77
Charlotte <huac15@zedat.fu-berlin.de>
2019-12-09 18:51:43 +0100

    minor fixes

--- index.js
+++ index.js
@@ -30,12 +30,16 @@ var features = svg.append("g")
 //Read and style Geodata
 d3.json("electr_estimates.geojson",function(error,geodata) {
   if (error) return console.log(error); //unknown error, check the console
+
+  
   var max_mwh = d3.max(geodata.features, function(d) { return d.properties.MWh});
   var min_mwh = d3.min(geodata.features, function(d) { return d.properties.MWh});
   var mean_mwh = d3.mean(geodata.features, function(d) { return d.properties.MWh});
   var med_mwh = d3.median(geodata.features, function(d) { return d.properties.MWh});
 
-  console.log(mean_mwh);
+  var total_mwh = d3.sum(geodata.features, function(d) { return d.properties.MWh});
+
+
   var color = d3.scale.linear()
                         .domain([min_mwh, med_mwh, mean_mwh, max_mwh])
                         .range(["#ffda66", "#ff9800", "#f67828", "#b60a1c"])
@@ -54,8 +58,61 @@ d3.json("electr_estimates.geojson",function(error,geodata) {
     .attr("fill", function(d) {
       return color(d.properties.MWh); 
       });
+      
+
+
+    // //Legend
+    // var defs = svg.append("defs");             
+
+    // var linearGradient = defs.append("linearGradient")
+    //                           .attr("id", "linear-gradient");
+
+    // //Set the color for the start (0%)
+    // linearGradient.append("stop")
+    //               .attr("offset", "0%")
+    //               .attr("stop-color", "#ffda66"); 
+
+    // //Set the color for the median (50%)
+    // linearGradient.append("stop")
+    //               .attr("offset", "50%")
+    //               .attr("stop-color", "#ff9800"); 
+
+    // //Set the color for the end (100%)
+    // linearGradient.append("stop")
+    //               .attr("offset", "100%")
+    //               .attr("stop-color", "#b60a1c"); 
+
+    // linearGradient.attr("x1", "0%")
+    //               .attr("y1", "100%")
+    //               .attr("x2", "0%")
+    //               .attr("y2", "0%");
+
+    // svg.append("rect")
+    //     .attr("width", width/55)
+    //     .attr("height", height - height/2)
+    //     .attr("id", "legend")
+    //     .style("fill", "url(#linear-gradient)");
+
+    // // Text
+
+    // var legend = svg.append("rect")
+    //                 .attr("width", width/30)
+    //                 .attr("height", height-height/2)
+    //                 .attr("fill", "white")
+    //                 .attr("x", 10)
+    //                 .style("text-anchor", "left");
+
+    // var max_string = Intl.NumberFormat('de-DEU').format((Math.round(max_mwh/1000))*1000) + String(" MWh");
+
+    // var text = svg.append("text")
+    //                   .text(max_string)
+    //                   .style("writing-mode", "vertical-lr")
+    //                   .attr('x', 20)
+    //                   .attr('fill', 'black')
+    //                   .attr("font-size", "x-small");
 
 });
+
 
 
 //Create tooltip
@@ -90,3 +147,8 @@ function hideTooltip() {
   tooltip.style("display","none");
 }
 
+
+
+
+
+  
