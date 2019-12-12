c03b00f37edd8716cd020162437f0de5feb84f5a
Charlotte <huac15@zedat.fu-berlin.de>
2019-12-12 09:09:44 +0100

    text change

--- index.js
+++ index.js
@@ -27,15 +27,81 @@ var rect =  d3.select("svg").append("rect")
 var features = svg.append("g")
     .attr("class","features");
 
+
+    
+
 //Read and style Geodata
 d3.json("electr_estimates_simplified.geojson",function(error,geodata) {
   if (error) return console.log(error); //unknown error, check the console
-
-  
+ 
   var max_mwh = Math.log10(d3.max(geodata.features, function(d) { return d.properties.MWh}));
   var min_mwh = Math.log10(d3.min(geodata.features, function(d) { return d.properties.MWh}));
   var mean_mwh = Math.log10(d3.mean(geodata.features, function(d) { return d.properties.MWh}));
   var med_mwh = Math.log10(d3.median(geodata.features, function(d) { return d.properties.MWh}));
+
+  //Legend
+  var defs = svg.append("defs");             
+  
+  var linearGradient = defs.append("linearGradient")
+                            .attr("id", "linear-gradient");
+
+  //Set the color for the start (0%)
+  linearGradient.append("stop")
+                .attr("offset", "0%")
+                .attr("stop-color", "#ffda66"); 
+
+  //Median
+  linearGradient.append("stop")
+      .attr("offset", "38.9%")
+      .attr("stop-color", "#ff9800"); 
+
+  //Mean
+  linearGradient.append("stop")
+      .attr("offset", "50.6%")
+      .attr("stop-color", "#f67828"); 
+
+  //Set the color for the end (100%)
+  linearGradient.append("stop")
+                .attr("offset", "100%")
+                .attr("stop-color", "#b60a1c"); 
+
+  linearGradient.attr("x1", "0%")
+                .attr("y1", "100%")
+                .attr("x2", "0%")
+                .attr("y2", "0%");
+
+
+  svg.append("rect")
+      .attr("width", width/55-3)
+      .attr("height", height - height/2 -50)
+      .attr("id", "legend")
+      .attr("rx", 5)
+      .attr("ry", 5)
+      .attr("x", width-10)
+      .style("fill", "url(#linear-gradient)");
+
+  //AXIS
+  var y = d3.scale.linear().range([0,  height-height/2 - 60]);
+  y.domain([max_mwh, min_mwh]);
+                  
+  var max_mwh_text = Math.round(d3.max(geodata.features, function(d) { return d.properties.MWh})/10000)*10000;
+  var min_mwh_text = Math.round(d3.min(geodata.features, function(d) { return d.properties.MWh}));
+
+  let tickLabels = [max_mwh_text, 10000, 500, min_mwh_text];
+
+  var yAxis = d3.svg.axis().scale(y)
+                    .orient("left").ticks(4)
+                    .tickValues([max_mwh, 4, 2.698970004, min_mwh])
+                    .tickFormat((d, i) => d3.format(",")(tickLabels[i]) + " MWh");
+
+  var g = svg.append("g")
+            .attr("class", "axis")
+            .style("fill", "#0a213e")
+            .call(yAxis)
+            .attr("transform", "translate(490, 6)");
+
+
+  //MAP
 
   var color = d3.scale.linear()
                         .domain([min_mwh, med_mwh, mean_mwh, max_mwh])
@@ -60,67 +126,7 @@ d3.json("electr_estimates_simplified.geojson",function(error,geodata) {
       
 
 
-    //Legend
-    var defs = svg.append("defs");             
-    
-    var linearGradient = defs.append("linearGradient")
-                              .attr("id", "linear-gradient");
-
-    //Set the color for the start (0%)
-    linearGradient.append("stop")
-                  .attr("offset", "0%")
-                  .attr("stop-color", "#ffda66"); 
-
-    //Median
-    linearGradient.append("stop")
-        .attr("offset", "38.9%")
-        .attr("stop-color", "#ff9800"); 
-
-    //Mean
-    linearGradient.append("stop")
-        .attr("offset", "50.6%")
-        .attr("stop-color", "#f67828"); 
-
-    //Set the color for the end (100%)
-    linearGradient.append("stop")
-                  .attr("offset", "100%")
-                  .attr("stop-color", "#b60a1c"); 
-
-    linearGradient.attr("x1", "0%")
-                  .attr("y1", "100%")
-                  .attr("x2", "0%")
-                  .attr("y2", "0%");
-
-
-    svg.append("rect")
-        .attr("width", width/55-3)
-        .attr("height", height - height/2 -50)
-        .attr("id", "legend")
-        .attr("rx", 5)
-        .attr("ry", 5)
-        .attr("x", width-10)
-        .style("fill", "url(#linear-gradient)");
-
-    //AXIS
-    var y = d3.scale.linear().range([0,  height-height/2 - 60]);
-    y.domain([max_mwh, min_mwh]);
-                   
-    var max_mwh_text = Math.round(d3.max(geodata.features, function(d) { return d.properties.MWh})/10000)*10000;
-    var min_mwh_text = Math.round(d3.min(geodata.features, function(d) { return d.properties.MWh}));
-
-    let tickLabels = [max_mwh_text, 10000, 500, min_mwh_text];
-
-    var yAxis = d3.svg.axis().scale(y)
-                      .orient("left").ticks(4)
-                      .tickValues([max_mwh, 4, 2.698970004, min_mwh])
-                      .tickFormat((d, i) => d3.format(",")(tickLabels[i]) + " MWh");
-
-    var g = svg.append("g")
-              .attr("class", "axis")
-              .style("fill", "#0a213e")
-              .call(yAxis)
-              .attr("transform", "translate(490, 6)");
-
+   
 });
 
 //Create tooltip
